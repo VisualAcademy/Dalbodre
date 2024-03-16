2d3b3e3d51445ddf180ab39b9c5931ee1d6819ba
Park YongJune <redplus@redplus.net>
2024-03-16 20:49:29 +0900

    Remove unnecessary usings

--- src/Dalbodre/Dalbodre/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
+++ src/Dalbodre/Dalbodre/Components/Account/IdentityComponentsEndpointRouteBuilderExtensions.cs
@@ -1,10 +1,7 @@
 using Dalbodre.Components.Account.Pages;
 using Dalbodre.Components.Account.Pages.Manage;
-using Dalbodre.Data;
 using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Http.Extensions;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Primitives;
 using System.Security.Claims;
