4d90c19c37c4da893f7aeb57c92002109834a4bf
Park YongJune <redplus@redplus.net>
2024-03-16 20:51:01 +0900

    Run Code Cleanup Ctrl K, Ctrl E

--- src/Dalbodre/Dalbodre/Components/Account/IdentityRevalidatingAuthenticationStateProvider.cs
+++ src/Dalbodre/Dalbodre/Components/Account/IdentityRevalidatingAuthenticationStateProvider.cs
@@ -1,7 +1,4 @@
-using Dalbodre.Data;
-using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using System.Security.Claims;
 
