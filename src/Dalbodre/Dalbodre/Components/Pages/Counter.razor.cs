5a709eac3b52e90745fe0de259c04b8d1ad1967c
Park YongJune <redplus@redplus.net>
2024-03-17 00:57:06 +0900

    Parameter

--- src/Dalbodre/Dalbodre/Components/Pages/Counter.razor.cs
+++ src/Dalbodre/Dalbodre/Components/Pages/Counter.razor.cs
@@ -1,12 +1,17 @@
+using Microsoft.AspNetCore.Components;
+
 namespace Dalbodre.Components.Pages
 {
     public partial class Counter
     {
         private int currentCount = 0;
 
+        [Parameter]
+        public int IncrementAmount { get; set; } = 1;
+
         private void IncrementCount()
         {
-            currentCount++;
+            currentCount += IncrementAmount;
         }
     }
 }
