74903b6eb7e91bcbbf02123f61ee234dcbb90d35
Park YongJune <redplus@redplus.net>
2024-03-19 00:11:03 +0900

    Run Code Cleanup

--- src/Dalbodre/Dalbodre/GlobalUsings.cs
+++ src/Dalbodre/Dalbodre/GlobalUsings.cs
@@ -1,7 +1,6 @@
-﻿global using Dalbodre.Components;
-global using Dalbodre.Components.Account;
+﻿global using Dalbodre.Components.Account;
 global using Dalbodre.Data;
 global using Microsoft.AspNetCore.Components.Authorization;
 global using Microsoft.AspNetCore.Identity;
-global using Microsoft.EntityFrameworkCore;
 global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
