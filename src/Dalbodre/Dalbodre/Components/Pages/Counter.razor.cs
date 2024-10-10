0529b9b171896d2628edbc7bb4941fe8f205dc3c
Park YongJune <redplus@redplus.net>
2024-10-10 19:35:56 +0900

    Convert to file-scoped namespace

--- src/Dalbodre/Dalbodre/Components/Pages/Counter.razor.cs
+++ src/Dalbodre/Dalbodre/Components/Pages/Counter.razor.cs
@@ -1,15 +1,14 @@
-namespace Dalbodre.Components.Pages
+namespace Dalbodre.Components.Pages;
+
+public partial class Counter
 {
-    public partial class Counter
+    private int currentCount = 0;
+
+    [Parameter]
+    public int IncrementAmount { get; set; } = 1;
+
+    private void IncrementCount()
     {
-        private int currentCount = 0;
-
-        [Parameter]
-        public int IncrementAmount { get; set; } = 1;
-
-        private void IncrementCount()
-        {
-            currentCount += IncrementAmount;
-        }
+        currentCount += IncrementAmount;
     }
 }
